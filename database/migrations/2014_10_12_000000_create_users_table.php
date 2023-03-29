c27ba692c526d426b55a6be7d078289888e380a6
James Favour <68597611+Sage-Ops-Core@users.noreply.github.com>
2023-03-29 22:40:15 +0100

    init

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        \Illuminate\Support\Facades\DB::statement('SET SESSION sql_require_primary_key=0');
+
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
